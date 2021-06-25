597c63633921fc0eb884bd529a8555c6acff96e6
yayoc <n.yayoshi+github@gmail.com>
2021-06-25 09:54:35 +0900

    add shell script to create a journal file

---
+++ scripts/append_date.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+DATE=$(date "+%Y%m%d")
+YESTERDAY=$(date -v -1d "+%Y%m%d")
+
+FILE_PATH="/Users/yayoc/Dropbox/Documents/Journal/$DATE.md"
+
+echo "# $DATE" >> $FILE_PATH
+echo "yesterday: [[$YESTERDAY]]" >> $FILE_PATH
+
