5863012c79ea9933cc5983fd1eb34f7fe7cfa3cb
yayoc <hi@yayoc.com>
2024-06-19 20:21:49 +0900

    Fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
-root_dir=$(dirname "$(pwd)")
-ln -s $root_dir/configs/nvim ~/.config/nvim
-ln -s $root_dir/configs/zsh/.zshrc ~/.zshrc
-ln -s $root_dir/configs/tmux/.tmux.conf ~/.tmux.conf
-ln -s $root_dir/configs/git/.gitconfig ~/.gitconfig
+script_dir=$(dirname "$(readlink -f "$0")")
+parent_dir=$(dirname "$script_dir")
+ln -s $parent_dir/configs/nvim ~/.config/nvim
+ln -s $parent_dir/configs/zsh/.zshrc ~/.zshrc
+ln -s $parent_dir/configs/tmux/.tmux.conf ~/.tmux.conf
+ln -s $parent_dir/configs/git/.gitconfig ~/.gitconfig
